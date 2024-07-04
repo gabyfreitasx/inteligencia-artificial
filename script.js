567cd378e33dda9cc9f8ed3da88cf13d504a7365
gabyfreitasx <133656430+gabyfreitasx@users.noreply.github.com>
2024-07-04 12:06:17 -0300

    pt 2

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você descobriu que está gravida",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Péssimo",
                 afirmacao: "afirmação"
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Se mata",
                 afirmacao: "afirmação"
             }
         ]
